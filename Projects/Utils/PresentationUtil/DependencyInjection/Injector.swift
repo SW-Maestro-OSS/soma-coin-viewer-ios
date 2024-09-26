da835a56ea5548a02c92d8580aa03aee57b67f14
J0onYEong <cjy2134@naver.com>
2024-09-26 18:00:47 +0900

    [CVW-002] [Feat] Injector와 Injected프로퍼티 랩퍼 구현

--- Projects/Utils/PresentationUtil/DependencyInjection/Injector.swift
+++ Projects/Utils/PresentationUtil/DependencyInjection/Injector.swift
@@ -5,5 +5,60 @@
 //  Created by choijunios on 9/26/24.
 //
 
-import Foundation
 import Swinject
+
+// MARK: Injected
+/// 의존성 주입을 간편하게 해주는 프로퍼티 랩퍼
+
+@propertyWrapper
+public class Injected<T> {
+    
+    public let wrappedValue: T
+    
+    public init() {
+        self.wrappedValue = DependencyInjector.shared.resolve()
+    }
+}
+
+// MARK: Injector
+public protocol DependencyAssemblable {
+    func assemble(_ assemblyList: [Assembly])
+    func register<T>(_ serviceType: T.Type, _ object: T)
+}
+
+
+public protocol DependencyResolvable {
+    func resolve<T>() -> T
+    func resolve<T>(_ serviceType: T.Type) -> T
+}
+
+public typealias Injector = DependencyAssemblable & DependencyResolvable
+
+public final class DependencyInjector: Injector {
+    
+    public static let shared: DependencyInjector = .init()
+    
+    private let container: Container
+    
+    private init(container: Container = Container()) {
+        self.container = container
+    }
+    
+    public func assemble(_ assemblyList: [Assembly]) {
+        assemblyList.forEach {
+            $0.assemble(container: container)
+        }
+    }
+    
+    public func register<T>(_ serviceType: T.Type, _ object: T) {
+        container.register(serviceType) { _ in object }
+    }
+    
+    public func resolve<T>() -> T {
+        container.resolve(T.self)!
+    }
+    
+    public func resolve<T>(_ serviceType: T.Type) -> T {
+        container.resolve(T.self)!
+    }
+}
