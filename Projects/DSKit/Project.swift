acd8295cd07bd7e6d6ef00af423838d601f0f445
J0onYEong <cjy2134@naver.com>
2024-10-14 22:35:53 +0900

    [CVW-004] [Feat] I18N 프로젝트 분리
    ResourceSynthesizer자동화 옵션이 프로젝트 단위라 DSKit에서 I18N을 분리했습니다.

--- Projects/DSKit/Project.swift
+++ Projects/DSKit/Project.swift
@@ -3,14 +3,6 @@ import DependencyPlugin
 
 let project = Project(
     name: "DSKit",
-    options: .options(
-        // Localization
-        defaultKnownRegions: ["en", "ko"],
-        developmentRegion: "ko"
-    ),
-    packages: [
-        .local(path: .relativeToRoot("LocalPackage/Macro/LocalizableMacro"))
-    ],
     targets: [
         .target(
             name: "CommonUI",
@@ -20,21 +12,10 @@ let project = Project(
             sources: ["./CommonUI/Sources/**"],
             resources: ["./CommonUI/Resources/**"],
             dependencies: [
-                .target(name: "I18N"),
                 .project(target: "PresentationUtil", path: "../Utils/PresentationUtil"),
+                .project(target: "I18N", path: "../I18N"),
             ]
         ),
-        .target(
-            name: "I18N",
-            destinations: .iOS,
-            product: .framework,
-            bundleId: "com.CoinViewer.DSKit.I18N",
-            sources: ["./I18N/Sources/**"],
-            resources: ["./I18N/Resources/**"],
-            dependencies: [
-                .package(product: "LocalizableMacro", type: .macro),
-            ]
-        )
     ]
 )
 
