5112c05b312fa99516ead9c568f214fa01d69ac7
J0onYEong <cjy2134@naver.com>
2024-10-14 14:40:00 +0900

    [CVW-004] [Feat] LS타입 정의

---
+++ Projects/DSKit/I18N/Sources/MainPageLocalizable.swift
@@ -0,0 +1,16 @@
+//
+//  MainPageLocalizable.swift
+//  I18N
+//
+//  Created by choijunios on 10/14/24.
+//
+
+import Foundation
+
+public extension LS {
+ 
+    enum MainPage {
+        
+        case Test
+    }
+}
