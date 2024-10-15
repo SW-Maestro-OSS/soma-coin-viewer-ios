bdc527d25e274fc9ddbe9e3ca601c41df7463586
J0onYEong <cjy2134@naver.com>
2024-10-15 20:02:34 +0900

    [CVW-006] [Feat] DefaultUserConfigurationService구현

--- Projects/Data/DataSource/Sources/Service/UserConfigurationService.swift
+++ Projects/Data/DataSource/Sources/Service/UserConfigurationService.swift
@@ -10,9 +10,22 @@ import Foundation
 public protocol UserConfigurationService {
     
     /// Configuration정보를 획득합니다.
-    func getConfiguration(key: String) -> String
+    func getConfiguration(key: String) -> String?
     
     
     /// Configuration정보를 설정합니다.
-    func setConfiguration(key: String, value: String) -> String
+    func setConfiguration(key: String, value: String)
+}
+
+public class DefaultUserConfigurationService: UserConfigurationService {
+    
+    private let source: UserDefaults = .standard
+    
+    public func getConfiguration(key: String) -> String? {
+        source.string(forKey: key)
+    }
+    
+    public func setConfiguration(key: String, value: String) {
+        source.set(value, forKey: key)
+    }
 }
