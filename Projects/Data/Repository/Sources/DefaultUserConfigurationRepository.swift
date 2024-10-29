0e00d771acee3b9df4a4d20e600558381aff646b
J0onYEong <cjy2134@naver.com>
2024-10-29 23:39:56 +0900

    [CVW-006] [Feat] 설정값을 캐싱하여 메모리에 저장

--- Projects/Data/Repository/Sources/DefaultUserConfigurationRepository.swift
+++ Projects/Data/Repository/Sources/DefaultUserConfigurationRepository.swift
@@ -19,42 +19,83 @@ public class DefaultUserConfigurationRepository: UserConfigurationRepository {
     @Injected var userConfigurationService: UserConfigurationService
     
     // Cache configuration
-    
+    private let cachedConfiguration: LockedDictionary<String, Any> = .init()
     
     public init() { }
     
     public func getCurrencyType() -> CurrencyType {
-        let key: UserConfiguration = .currency
+        let config: UserConfiguration = .currency
         
-        guard let currencyString = userConfigurationService
-            .getConfiguration(key: key.savingKey) else {
-            
-            return .defaultValue
+        if let memoryCached: String = checkMemoryCache(key: config.savingKey) {
+            // 캐싱된 정보를 먼저 확인합니다.
+            return .init(rawValue: memoryCached)!
         }
         
-        return .init(rawValue: currencyString)!
+        if let diskCached = userConfigurationService.getConfiguration(key: config.savingKey) {
+            // 로컬에 저장된 정보를 확인합니다.
+            
+            // 정보를 메모리에 캐싱
+            cachedConfiguration[config.savingKey] = diskCached
+            
+            return .init(rawValue: diskCached)!
+        }
+        
+        return .init(rawValue: config.defaultSavingValue)!
     }
+    
+    
     
     public func setCurrencyType(type: Domain.CurrencyType) {
-        let key: UserConfiguration = .currency
-        userConfigurationService.setConfiguration(key: key.savingKey, value: type.savingValue)
+        let config: UserConfiguration = .currency
+        
+        // 디스크 저장
+        userConfigurationService.setConfiguration(key: config.savingKey, value: type.savingValue)
+        
+        // 메모리 저장
+        cachedConfiguration[config.savingKey] = type.savingValue
     }
+    
+    
     
     public func getGridType() -> Domain.GridType {
+        let config: UserConfiguration = .gridType
         
-        let key: UserConfiguration = .gridType
-        
-        guard let gridString = userConfigurationService
-            .getConfiguration(key: key.savingKey) else {
-            
-            return .defaultValue
+        if let memoryCached: String = checkMemoryCache(key: config.savingKey) {
+            // 캐싱된 정보를 먼저 확인합니다.
+            return .init(rawValue: memoryCached)!
         }
         
-        return .init(rawValue: gridString)!
+        if let diskCached = userConfigurationService.getConfiguration(key: config.savingKey) {
+            // 로컬에 저장된 정보를 확인합니다.
+            
+            // 정보를 메모리에 캐싱
+            cachedConfiguration[config.savingKey] = diskCached
+            
+            return .init(rawValue: diskCached)!
+        }
+        
+        return .init(rawValue: config.defaultSavingValue)!
     }
     
+    
+    
     public func setGrideType(type: Domain.GridType) {
-        let key: UserConfiguration = .gridType
-        userConfigurationService.setConfiguration(key: key.savingKey, value: type.savingValue)
+        let config: UserConfiguration = .gridType
+        
+        // 디스크 저장
+        userConfigurationService.setConfiguration(key: config.savingKey, value: type.savingValue)
+        
+        // 메모리 저장
+        cachedConfiguration[config.savingKey] = type.savingValue
+    }
+    
+    
+    
+    // MARK: check cache
+    private func checkMemoryCache<T>(key: String) -> T? {
+        if Thread.isMainThread {
+            printIfDebug("메인쓰레드에서 LockedDictionary에 접근하고 있다, 해당동작은 메인쓰레드를 블로킹(NSLock)할 수 있다.")
+        }
+        return cachedConfiguration[key] as? T
     }
 }
