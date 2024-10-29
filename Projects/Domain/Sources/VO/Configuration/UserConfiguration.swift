207d5a21884081c8cca6fc2edb6c4673fd7023af
J0onYEong <cjy2134@naver.com>
2024-10-29 23:39:56 +0900

    [CVW-006] [Feat] 설정값을 캐싱하여 메모리에 저장

--- Projects/Domain/Sources/VO/Configuration/UserConfiguration.swift
+++ Projects/Domain/Sources/VO/Configuration/UserConfiguration.swift
@@ -7,34 +7,40 @@
 
 import Foundation
 
+/// 설정 옵션의 종류를 의미합니다.
 public enum UserConfiguration: String {
     
     case currency
     case gridType
+    
+    public var defaultSavingValue: String {
+        switch self {
+        case .currency:
+            CurrencyType.dollar.savingValue
+        case .gridType:
+            GridType.list.savingValue
+        }
+    }
     
     public var savingKey: String {
         "configuration_\(self.rawValue)"
     }
 }
 
-// MARK: Grid type
+// MARK: Grid type, 그리드 타입
 public enum GridType: String {
     case list="LIST"
     case twoByTwo="2X2"
-    
-    public static let defaultValue: GridType = .list
-    
+
     public var savingValue: String {
         self.rawValue
     }
 }
 
-// MARK: Currency
+// MARK: Currency, 화폐 타입
 public enum CurrencyType: String {
     case won="WON"
     case dollar="DOLLAR"
-    
-    public static let defaultValue: CurrencyType = .won
     
     public var savingValue: String {
         self.rawValue
