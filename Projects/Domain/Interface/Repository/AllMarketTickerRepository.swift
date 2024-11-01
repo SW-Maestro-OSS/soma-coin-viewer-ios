603eaad6b76f13e0e684eec4963b1bf150cc40cf
J0onYEong <cjy2134@naver.com>
2024-11-01 19:35:33 +0900

    [CVW-009] [Feat] AllMarketTickerRepository생성

---
+++ Projects/Domain/Interface/Repository/AllMarketTickerRepository.swift
@@ -0,0 +1,13 @@
+//
+//  AllMarketTickerRepository.swift
+//  DomainInterface
+//
+//  Created by choijunios on 11/1/24.
+//
+
+import Foundation
+
+public protocol AllMarketTickerRepository {
+    
+    
+}
