0f632843d23afba3823b8dda6870c23cc2e58afb
J0onYEong <cjy2134@naver.com>
2024-11-02 16:24:03 +0900

    [CVW-009] [Feat] DefaultAllMarketTickerPageUseCase구현

---
+++ Projects/Domain/Concrete/UseCase/DefaultAllMarketTickerPageUseCase.swift
@@ -0,0 +1,43 @@
+//
+//  DefaultAllMarketTickerPageUseCase.swift
+//  Domain
+//
+//  Created by choijunios on 11/2/24.
+//
+
+import Foundation
+import Combine
+
+import DomainInterface
+import CoreUtil
+
+public class DefaultAllMarketTickerPageUseCase: AllMarketTickerPageUseCase {
+    
+    @Injected var allMarketTickerRepository: (any AllMarketTickerRepository)
+    
+    public init() { }
+    
+    public func getList(count: Int) -> AnyPublisher<[Symbol24hTickerVO], Never> {
+        
+        allMarketTickerRepository
+            .subscribe()
+            .throttle(for: 0.35, scheduler: DispatchQueue.main, latest: true)
+            .catch { error in
+                
+                // 에러 처리
+                
+                return Just([])
+            }
+            .map { tickers in
+                
+                // QuoteAssetVolume을 사용하요 내림차순 정렬
+                let sortedTickers = tickers.sorted { lhs, rhs in
+                    lhs.totalTradedQuoteAssetVolume > rhs.totalTradedQuoteAssetVolume
+                }
+                
+                // 상위 30개만 전달
+                return Array(sortedTickers[0..<30])
+            }
+            .eraseToAnyPublisher()
+    }
+}
--- Projects/Features/AllMarketTicker/Example/Sources/DI/DataAssembly.swift
+++ Projects/Features/AllMarketTicker/Example/Sources/DI/DataAssembly.swift
@@ -26,8 +26,22 @@ public class DataAssembly: Assembly {
         
         
         // MARK: Repository
-        container.register(AllMarketTickerRepository.self) { _ in
+        container.register(WebSocketConfiguable.self) { _ in
+            WebSocketConfiguration()
+        }
+        .inObjectScope(.container)
+        
+        container.register((any AllMarketTickerRepository).self) { _ in
             DefaultAllMarketTickerRepository()
         }
     }
+}
+
+public class WebSocketConfiguration: WebSocketConfiguable {
+    
+    public let baseURL: String = "wss://stream.binance.com:443/ws"
+    
+    public let streamName: [WebSocketStream : String] = [
+        .allMarketTickers : "!ticker@arr"
+    ]
 }
