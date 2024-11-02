6c9ca12c1c016e088a8daed838f53803289e7767
J0onYEong <cjy2134@naver.com>
2024-11-02 14:18:59 +0900

    [CVW-009] [Feat] WebSocketHelper구현 및 주입

--- Projects/Domain/Interface/Repository/StreamConnectable/StreamConnectable.swift
+++ Projects/Domain/Interface/Repository/StreamConnectable/StreamConnectable.swift
@@ -12,15 +12,9 @@ public protocol StreamConnectable {
     
     associatedtype Element
     
-    /// 웹소캣을 연결합니다.
-    func connect(completion: (Error?) -> ())
-    
     /// 구독을 실행합니다.
     func subscribe() -> AnyPublisher<Element, Error>
     
     /// 구독을 종료합니다.
     func unsubscribe()
-    
-    /// 웹소켓 연결을 종료합니다.
-    func disconnect()
 }
