1e66e1ce28c46aa6af388e1edbf07604b28bc400
J0onYEong <cjy2134@naver.com>
2024-12-05 22:21:26 +0900

    [CVW-010] [Feat] TickerSortComparator 정의

---
+++ Projects/Features/AllMarketTicker/Feature/Sources/Model/TickerSortComparator.swift
@@ -0,0 +1,15 @@
+//
+//  TickerSortComparator.swift
+//  AllMarketTickerModule
+//
+//  Created by choijunios on 12/5/24.
+//
+
+import DomainInterface
+
+protocol TickerSortComparator {
+    
+    typealias TickerVO = Twenty4HourTickerForSymbolVO
+    
+    func compare(lhs: TickerVO, rhs: TickerVO) -> Bool
+}
