e751af3d0c0982c2516ff5a15e764b76a5c2d1e7
J0onYEong <cjy2134@naver.com>
2024-12-05 22:08:48 +0900

    [CVW-010] [Feat] UDFObservableObject정의
    단방향 아키텍처를 편하게 도입하기 위해 구현했습니다.

---
+++ Projects/Features/Base/Interface/Navigation/UDFViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  UDFViewModel.swift
+//  BaseModule
+//
+//  Created by choijunios on 12/5/24.
+//
+
+import SwiftUI
+import Combine
+
+import CoreUtil
+
+public typealias UDFObservableObject = UDFViewModel & ObservableObject
+
+public protocol UDFViewModel: AnyObject {
+    
+    associatedtype State
+    associatedtype Action
+    
+    var action: PassthroughSubject<Action, Never> { get }
+    var state: State { get set }
+    var store: Set<AnyCancellable> { get set }
+    
+    func mutate(_ action: Action) -> AnyPublisher<Action, Never>
+    
+    func reduce(_ action: Action, state: State) -> State
+}
+
+public extension UDFViewModel {
+    
+    func createStateStream() {
+        
+        action
+            .unretained(self)
+            .flatMap { viewModel, action in
+                
+                // #1. Mutation for side effect
+                
+                return viewModel.mutate(action)
+            }
+            .receive(on: DispatchQueue.main)
+            .unretained(self)
+            .sink { viewModel, action in
+                
+                // #2. Update current state
+                
+                let currentState = viewModel.state
+                viewModel.state = viewModel.reduce(action, state: currentState)
+            }
+            .store(in: &store)
+        
+    }
+}
