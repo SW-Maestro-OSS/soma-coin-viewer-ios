3e5c27a2c87bdcecd2ecbf070b27e1c89cf64b6c
J0onYEong <cjy2134@naver.com>
2024-12-06 14:53:05 +0900

    [CVW-010] [Feat] CVText구현

--- Projects/Features/AllMarketTicker/Feature/Sources/View/TickerSortSelectorView.swift
+++ Projects/Features/AllMarketTicker/Feature/Sources/View/TickerSortSelectorView.swift
@@ -7,4 +7,25 @@
 
 import SwiftUI
 
+import CommonUI
+
+struct TickerSortSelectorView: View {
+    
+    @StateObject var viewModel: TickerSortSelectorViewModel
+    
+    init(viewModel: TickerSortSelectorViewModel) {
+        
+        self._viewModel = StateObject(wrappedValue: viewModel)
+    }
+    
+    var body: some View {
+        
+        HStack {
+            
+            Image(systemName: "chevron.up.chevron.down")
+            
+            CVText(text: $viewModel.state.title)
+        }
+    }
+}
 
