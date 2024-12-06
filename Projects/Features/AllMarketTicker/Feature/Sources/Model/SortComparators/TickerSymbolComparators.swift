fc87d098f80086881d6d48a86068237744f4bfbf
J0onYEong <cjy2134@naver.com>
2024-12-06 16:03:36 +0900

    [CVW-010] [Feat] 정렬조건 구현 및 화면상에 그리드로 표현

---
+++ Projects/Features/AllMarketTicker/Feature/Sources/Model/SortComparators/TickerSymbolComparators.swift
@@ -0,0 +1,75 @@
+//
+//  TickerSymbolComparators.swift
+//  AllMarketTickerModule
+//
+//  Created by choijunios on 12/6/24.
+//
+
+import DomainInterface
+
+// MARK: Symbol
+
+struct TickerSymbolAscendingComparator: TickerSortComparator {
+    
+    let id: String = String(describing: TickerSymbolAscendingComparator.self)
+    
+    func compare(lhs: TickerVO, rhs: TickerVO) -> Bool {
+        
+        lhs.symbol < rhs.symbol
+    }
+}
+
+struct TickerSymbolDescendingComparator: TickerSortComparator {
+    
+    let id: String = String(describing: TickerSymbolDescendingComparator.self)
+    
+    func compare(lhs: TickerVO, rhs: TickerVO) -> Bool {
+        
+        lhs.symbol > rhs.symbol
+    }
+}
+
+// MARK: Price
+
+struct TickerPriceAscendingComparator: TickerSortComparator {
+    
+    let id: String = String(describing: TickerPriceAscendingComparator.self)
+    
+    func compare(lhs: TickerVO, rhs: TickerVO) -> Bool {
+        
+        lhs.price < rhs.price
+    }
+}
+
+struct TickerPriceDescendingComparator: TickerSortComparator {
+    
+    let id: String = String(describing: TickerPriceDescendingComparator.self)
+    
+    func compare(lhs: TickerVO, rhs: TickerVO) -> Bool {
+        
+        lhs.price > rhs.price
+    }
+}
+
+// MARK: 24hChangePercent
+
+struct Ticker24hChangeAscendingComparator: TickerSortComparator {
+    
+    let id: String = String(describing: Ticker24hChangeAscendingComparator.self)
+    
+    func compare(lhs: TickerVO, rhs: TickerVO) -> Bool {
+        
+        lhs.changedPercent < rhs.changedPercent
+    }
+}
+
+struct Ticker24hChangeDescendingComparator: TickerSortComparator {
+    
+    let id: String = String(describing: Ticker24hChangeDescendingComparator.self)
+    
+    func compare(lhs: TickerVO, rhs: TickerVO) -> Bool {
+        
+        lhs.changedPercent > rhs.changedPercent
+    }
+}
+
