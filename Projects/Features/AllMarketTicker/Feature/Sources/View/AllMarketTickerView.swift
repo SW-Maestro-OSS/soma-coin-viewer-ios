1ddde5d3fe8a1eab9857ae1037f01a52665f7673
J0onYEong <cjy2134@naver.com>
2024-12-07 13:14:27 +0900

    [CVW-014] [Chore] 뷰코드 정리

--- Projects/Features/AllMarketTicker/Feature/Sources/View/AllMarketTickerView.swift
+++ Projects/Features/AllMarketTicker/Feature/Sources/View/AllMarketTickerView.swift
@@ -14,12 +14,6 @@ public struct AllMarketTickerView: View {
     
     @StateObject private var viewModel: AllMarketTickerViewModel = .init()
     
-    private let columns: [GridItem] = [
-        GridItem(.flexible()),
-        GridItem(.flexible()),
-        GridItem(.flexible()),
-    ]
-    
     public init() { }
     
     public var body: some View {
@@ -27,23 +21,9 @@ public struct AllMarketTickerView: View {
         VStack {
             
             // MARK: Sort selection
-            LazyVGrid(columns: columns, spacing: 0) {
-                
-                ForEach(viewModel.state.sortCompartorViewModels) { viewModel in
-                    
-                    TickerSortSelectorView(viewModel: viewModel)
-                        .frame(height: 45)
-                }
-            }
-            .background {
-                Rectangle()
-                    .foregroundStyle(.gray.opacity(0.3))
-                    .background { Rectangle().foregroundStyle(.white) }
-                    .padding(.vertical, 1)
-                    .background {
-                        Rectangle().foregroundStyle(.black)
-                    }
-            }
+            SortSelectionContainerView(
+                sortSelectionViewModels: $viewModel.state.sortCompartorViewModels
+            )
             
             // MARK: Ticker list
             AllMarketTickerListView(
---
+++ Projects/Features/AllMarketTicker/Feature/Sources/View/Components/SortSelectionContainerView.swift
@@ -0,0 +1,40 @@
+//
+//  SortSelectionContainerView.swift
+//  AllMarketTickerModule
+//
+//  Created by choijunios on 12/7/24.
+//
+
+import SwiftUI
+
+struct SortSelectionContainerView: View {
+    
+    @Binding var sortSelectionViewModels: [TickerSortSelectorViewModel]
+    
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    
+    var body: some View {
+        
+        LazyVGrid(columns: columns, spacing: 0) {
+            
+            ForEach(sortSelectionViewModels) { viewModel in
+                
+                TickerSortSelectorView(viewModel: viewModel)
+                    .frame(height: 45)
+            }
+        }
+        .background {
+            Rectangle()
+                .foregroundStyle(.gray.opacity(0.3))
+                .background { Rectangle().foregroundStyle(.white) }
+                .padding(.vertical, 1)
+                .background {
+                    Rectangle().foregroundStyle(.black)
+                }
+        }
+    }
+}
