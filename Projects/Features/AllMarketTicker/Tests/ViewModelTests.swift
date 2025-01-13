b156dc9e6b4dcf5be90bcf2971f9e8e00b95799d
J0onYEong <cjy2134@naver.com>
2025-01-13 20:05:15 +0900

    [CVM-000] [Refactor] AllMarketTicker테스트 코드 수정

--- Projects/Features/AllMarketTicker/Tests/ViewModelTests.swift
+++ Projects/Features/AllMarketTicker/Tests/ViewModelTests.swift
@@ -5,24 +5,22 @@
 
 import Testing
 
-import AllMarketTickerFeatureTesting
+@testable import AllMarketTickerFeatureTesting
 @testable import AllMarketTickerFeature
 
 import DomainInterface
 import CoreUtil
 
-/// 테스트 제목: AllmarketTicker의 특정항목을 잘 정렬하는지 검사
-///
-/// 
-
+/// 테스트 제목: AllmarketTicker의 선택된 정렬 기준이 올바르게 적용되는지 확인
 @Test
 func sortingTest() {
     
+    // Given
     let viewModel = AllMarketTickerViewModel(
-        socketHelper: MockWebSocketHelper(),
-        useCase: MockAllMarketTickersUseCase()
+        socketHelper: FakeWebSocketHelper(),
+        useCase: FakeAllMarketTickersUseCase(),
+        userConfigurationRepository: StubUserConfigurationRepository()
     )
-    
     let testTickers: [Twenty4HourTickerForSymbolVO] = [
         Twenty4HourTickerForSymbolVO(pairSymbol: "test1USDT", price: 400.0, totalTradedQuoteAssetVolume: 1.0, changedPercent: 1.0),
         Twenty4HourTickerForSymbolVO(pairSymbol: "test2USDT", price: 100.0, totalTradedQuoteAssetVolume: 1.0, changedPercent: 1.0),
@@ -35,18 +33,18 @@ func sortingTest() {
         
         return newTicker
     }
-    
     let initialState: AllMarketTickerViewModel.State = .init(
-        tickerList: testTickers
+        tickerList: testTickers,
+        tickerDisplayType: .list
     )
-    
-    let testComparator = TestSortComparator()
-    
+    let testComparator = StubSortComparator()
     let expectPriceListResult: [CVNumber] = [100.0, 200.0, 300.0, 400.0]
     
-    
+    // When
     let resultState = viewModel.reduce(.changeSortingCriteria(comparator: testComparator), state: initialState)
-    let priceList = resultState.tickerList.map { $0.price }
     
+    
+    // Then
+    let priceList = resultState.tickerList.map { $0.price }
     #expect(expectPriceListResult == priceList)
 }
