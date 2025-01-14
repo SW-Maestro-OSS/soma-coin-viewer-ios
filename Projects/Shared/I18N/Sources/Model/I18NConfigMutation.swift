02ed22424f8dba8e83563249cf99a535d8255626
J0onYEong <cjy2134@naver.com>
2025-01-14 20:32:08 +0900

    [CVM-020] [Feat] 상태변화를 퍼블리싱 하도록 변경

---
+++ Projects/Shared/I18N/Sources/Model/I18NConfigMutation.swift
@@ -0,0 +1,14 @@
+//
+//  I18NConfigMutation.swift
+//  I18N
+//
+//  Created by choijunios on 1/14/25.
+//
+
+import DomainInterface
+
+public struct I18NConfigMutation {
+    
+    public var languageType: LanguageType?
+    public var currencyType: CurrencyType?
+}
