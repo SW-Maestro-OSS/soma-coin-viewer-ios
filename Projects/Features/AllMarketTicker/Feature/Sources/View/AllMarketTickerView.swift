79a0cf55a5e082db30b4cf11a36149061f158aca
J0onYEong <cjy2134@naver.com>
2025-01-23 14:56:23 +0900

    [CVW-025] Chore, 주석 수정

--- Projects/Features/AllMarketTicker/Feature/Sources/View/AllMarketTickerView.swift
+++ Projects/Features/AllMarketTicker/Feature/Sources/View/AllMarketTickerView.swift
@@ -32,7 +32,7 @@ struct AllMarketTickerView: View {
         
         VStack(spacing: 0) {
             
-            // MARK: Sort selection
+            // MARK: Sort selection view
             LazyVGrid(columns: columns, spacing: 0) {
                 ForEach(viewModel.state.displayingSortSelectionCellROs) { ro in
                     Group {
