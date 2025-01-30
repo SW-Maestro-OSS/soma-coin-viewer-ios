b8230f8bd8aeac0e1358e0285e46399950ceb373
J0onYEong <cjy2134@naver.com>
2025-01-30 18:09:54 +0900

    [CVW-030] Feat, AlertShooter모듈 생성

---
+++ Projects/Shared/AlertShooter/Project.swift
@@ -0,0 +1,51 @@
+//
+//  Project.swift
+//
+//  Created by choijunios on 2025/01/30
+//
+
+import ProjectDescription
+
+import ConfigurationPlugin
+import DependencyPlugin
+
+let project = Project(
+    name: "AlertShooter",
+    targets: [
+        
+        .target(
+            name: "AlertShooterTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "com.CoinViewer.shared.AlertShooter.tests",
+            infoPlist: .default,
+            sources: ["Tests/**"],
+            dependencies: [
+                .target(name: "AlertShooter"),
+                .target(name: "AlertShooterTesting"),
+            ]
+        ),
+        
+        .target(
+            name: "AlertShooterTesting",
+            destinations: .iOS,
+            product: .staticLibrary,
+            bundleId: "com.CoinViewer.shared.AlertShooter.testing",
+            sources: ["Testing/**"],
+            dependencies: [
+                .target(name: "AlertShooter"),
+            ]
+        ),
+        
+        .target(
+            name: "AlertShooter",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "com.choijunios.shared.AlertShooter",
+            sources: ["Sources/**"],
+            dependencies: [
+                D.Util.CoreUtil,
+            ]
+        ),
+    ]
+)
