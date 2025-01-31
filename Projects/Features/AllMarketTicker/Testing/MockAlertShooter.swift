f97523432b28dd4ee357acd0a6ffd2394e657659
J0onYEong <cjy2134@naver.com>
2025-01-31 18:24:47 +0900

    [CVW-030] Feat, AllMarketTicker의존성 지정 및 Mock객체 생성 및 주입

---
+++ Projects/Features/AllMarketTicker/Testing/MockAlertShooter.swift
@@ -0,0 +1,21 @@
+//
+//  MockAlertShooter.swift
+//  AllMarketTickerModule
+//
+//  Created by choijunios on 1/31/25.
+//
+
+import AlertShooter
+
+class MockAlertShooter: AlertShooter {
+    
+    var shotAlertModels: [AlertModel] = []
+    
+    func request(listener: AlertShooterListener) {
+        fatalError()
+    }
+    
+    func shoot(_ model: AlertModel) {
+        shotAlertModels.append(model)
+    }
+}
