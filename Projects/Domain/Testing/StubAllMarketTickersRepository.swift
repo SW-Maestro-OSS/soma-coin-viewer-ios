4f086fa5729792d7b8c097e2dc603c0d88703adf
J0onYEong <cjy2134@naver.com>
2025-04-22 17:56:29 +0900

    [CVW-040] Feat, 불필요 객체 제거

--- Projects/Domain/Testing/StubAllMarketTickersRepository.swift
+++ Projects/Domain/Testing/StubAllMarketTickersRepository.swift
@@ -10,18 +10,3 @@ import Foundation
 
 import DomainInterface
 import CoreUtil
-
-class StubAllMarketTickersRepository: AllMarketTickersRepository {
-    
-    let stubTickerEntities: [Twenty4HourTickerForSymbolVO]
-    
-    init(stubTickerEntities: [Twenty4HourTickerForSymbolVO]) {
-        self.stubTickerEntities = stubTickerEntities
-    }
-    
-    func request24hTickerForAllSymbols() -> AnyPublisher<[Twenty4HourTickerForSymbolVO], Never> {
-        
-        return Just(stubTickerEntities).eraseToAnyPublisher()
-    }
-}
-
