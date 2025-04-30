8c5239ccdf27ed9f3d80f27553db1813cc37c3e2
J0onYEong <cjy2134@naver.com>
2025-04-30 16:11:51 +0900

    [CVW-000] Root, Tab Builder의존성 생성 시점 수정

--- Projects/Features/Root/Feature/Sources/RootBuilder.swift
+++ Projects/Features/Root/Feature/Sources/RootBuilder.swift
@@ -18,18 +18,13 @@ import AlertShooter
 import CoreUtil
 
 public final class RootBuilder {
-    // Dependency
-    @Injected private var rootPageUseCase: RootPageUseCase
-    @Injected private var alertShooter: AlertShooter
-    @Injected private var i18NManager: I18NManager
-
     public init() { }
     
     public func build() -> RootRoutable {
         let viewModel = RootViewModel(
-            useCase: rootPageUseCase,
-            alertShooter: alertShooter,
-            i18NManager: i18NManager
+            useCase: DependencyInjector.shared.resolve(),
+            alertShooter: DependencyInjector.shared.resolve(),
+            i18NManager: DependencyInjector.shared.resolve()
         )
         let view = RootView(viewModel: viewModel)
         let tabBarBuilder = TabBarBuilder()
