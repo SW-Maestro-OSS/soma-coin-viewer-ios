dd8f18526b1fbc685dbfdfdac7da84aaf38c2a52
J0onYEong <cjy2134@naver.com>
2025-06-05 20:45:37 +0900

    [CVW-049] Refactor, TickerCellRO불필요 프로퍼티 삭제

--- Projects/Features/AllMarketTicker/Feature/Sources/Model/RenderObject/TickerCellRO.swift
+++ Projects/Features/AllMarketTicker/Feature/Sources/Model/RenderObject/TickerCellRO.swift
@@ -11,18 +11,9 @@ import CoreUtil
 
 struct TickerCellRO: Identifiable {
     var id: String { self.symbolText }
-    
     let symbolText: String
     let symbolImageURL: String
-    
-    var displayPriceText: String
-    var displayChangePercentText: String
-    var displayChangePercentTextColor: Color
-    
-    // For sort
-    let symbolPair: String
-    let price: CVNumber
-    let changedPercent: CVNumber
-    let firstSymbol: String
-    let secondSymbol: String
+    var priceText: String
+    var changePercentText: String
+    var changePercentTextColor: Color
 }
