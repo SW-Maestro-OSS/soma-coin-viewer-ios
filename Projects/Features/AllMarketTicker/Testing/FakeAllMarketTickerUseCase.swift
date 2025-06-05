51ccb12d7d48f8613cf335f8bb4bcb7d6f532398
J0onYEong <cjy2134@naver.com>
2025-06-05 21:12:57 +0900

    [CVW-049] Feat, FakeAllMarketTicker대역 수정

--- Projects/Features/AllMarketTicker/Testing/FakeAllMarketTickerUseCase.swift
+++ Projects/Features/AllMarketTicker/Testing/FakeAllMarketTickerUseCase.swift
@@ -5,6 +5,7 @@
 //  Created by choijunios on 4/24/25.
 //
 
+import Foundation
 import Combine
 
 import DomainInterface
@@ -14,44 +15,19 @@ public final class FakeAllMarketTickerUseCase: AllMarketTickersUseCase {
     
     public init() { }
     
-    public func getTickerList(rowCount: UInt) -> AnyPublisher<[DomainInterface.Twenty4HourTickerForSymbolVO], Never> {
-        let list = (0..<rowCount).map { index in
-            var vo = Twenty4HourTickerForSymbolVO(
+    public func getTickerListStream(tickerCount: Int) -> AnyPublisher<TickerList, Never> {
+        let tickers = (0..<tickerCount).map { index in
+            Ticker(
                 pairSymbol: "BTC\(index)"+"USDT",
-                price: CVNumber(Double(100 * index)),
-                totalTradedQuoteAssetVolume: CVNumber(Double(100 * index)),
-                changedPercent: CVNumber(Double(100 * index)),
-                bestBidPrice: CVNumber(Double(100 * index)),
-                bestAskPrice: CVNumber(Double(100 * index))
+                price: Decimal(100 * index),
+                totalTradedQuoteAssetVolume: Decimal(100 * index),
+                changedPercent: Decimal(100 * index),
+                bestBidPrice: Decimal(100 * index),
+                bestAskPrice: Decimal(100 * index)
             )
-            vo.setSymbols { pairSymbol in
-                ("BTC\(index)", "USDT")
-            }
-            return vo
         }
-        let publisher = CurrentValueSubject<[Twenty4HourTickerForSymbolVO], Never>(list)
+        let publisher = CurrentValueSubject<TickerList, Never>(TickerList(currencyType: .dollar, tickers: tickers))
         return publisher.eraseToAnyPublisher()
-    }
-    
-    public func getTickerList(rowCount: UInt) async -> [DomainInterface.Twenty4HourTickerForSymbolVO] {
-        (0..<rowCount).map { index in
-            var vo = Twenty4HourTickerForSymbolVO(
-                pairSymbol: "BTC\(index)"+"USDT",
-                price: CVNumber(Double(100 * index)),
-                totalTradedQuoteAssetVolume: CVNumber(Double(100 * index)),
-                changedPercent: CVNumber(Double(100 * index)),
-                bestBidPrice: CVNumber(Double(100 * index)),
-                bestAskPrice: CVNumber(Double(100 * index))
-            )
-            vo.setSymbols { pairSymbol in
-                ("BTC\(index)", "USDT")
-            }
-            return vo
-        }
-    }
-    
-    public func getExchangeRate(base: DomainInterface.CurrencyType, to: DomainInterface.CurrencyType) async -> Double? {
-        1.0
     }
     
     public func getGridType() -> GridType { .list }
